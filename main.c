eff57c483c0332c24f603283f2d564c28d25d7bb
soo0300 <soo0300@naver.com>
2022-11-27 17:53:27 +0900

    실습_1(수정)

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define MAX_NAME 20
-int main(int argc, char *argv[]) {
-	struct student{
-		int ID;
-		char name[MAX_NAME];
-		double grade;			
-	};
-	
-	int main(void){
-		struct student std1;
-		//struct student std2 = {1511104,"coco",4.3};
-	
-		
-		std1.ID=1811103;
-		strcpy(std1.name,"soojin");
-		std1.grade=4.3;
 
-	}
+struct student{
+	int ID;
+	char name[MAX_NAME];
+	double grade;
+};
+
+int main(void){
+	struct student std1 = {1511104,"coco",4.3};
+
 	
-	return 0;
+	std1.ID=1811103;
+	strcpy(std1.name,"soojin");
+	std1.grade=4.3;
+	std1.grade=4.5;
+		
+	printf("ID: %i\n",std1.ID);
+	printf("name: %s\n",std1.name);
+	printf("grade: %.1f\n",std1.grade);
+
 }
