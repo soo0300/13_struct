c91b31ed599491158d4c3205e48c657e4469eb58
soo0300 <soo0300@naver.com>
2022-11-27 18:03:16 +0900

    실습_2

--- main.c
+++ main.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<math.h>
 #define MAX_NAME 20
 
-struct student{
-	int ID;
-	char name[MAX_NAME];
-	double grade;
+struct point{
+	int x;
+	int y;
 };
 
 int main(void){
-	struct student std1 = {1511104,"coco",4.3};
-
+	struct point p1,p2;
+	int xdiff, ydiff;
+	double dist;
 	
-	std1.ID=1811103;
-	strcpy(std1.name,"soojin");
-	std1.grade=4.3;
-	std1.grade=4.5;
-		
-	printf("ID: %i\n",std1.ID);
-	printf("name: %s\n",std1.name);
-	printf("grade: %.1f\n",std1.grade);
+	printf("input p1 coordinate(x,y): ");
+	scanf("%d %d",&p1.x,&p1.y);
+	
+	printf("input p2 coordinate(x,y): ");
+	scanf("%d %d",&p2.x,&p2.y);
+	
+	xdiff = p2.x-p1.x;
+	ydiff = p2.y-p1.y;
+	
+	dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+	
+	printf("distance: %f\n",dist);
+	
+	
+	
+	
+	
+	return 0;
+
 
 }
